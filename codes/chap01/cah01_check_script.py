6471abc2ad377a0bac760882be4ce2c80b77b07a
Baek-Lok Oh <rocks529@gmail.com>
2020-03-09 17:28:23 +0900

    first process

--- codes/chap01/cah01_check_script.py
+++ codes/chap01/cah01_check_script.py
@@ -1,5 +1,7 @@
 #-*- coding:utf-8 -*-
 
+"""docstring - my own code for a simple perceptron """
+
 import numpy as np
 #import csv
 import pandas as pd
@@ -20,29 +22,36 @@ def load_dataset():
     df['Indetermined'] = np.where(df['Sex'] == 'I', 1, 0)
     del df['Sex']
 
+def model_init():
+    global weight, bias
+    weight = np.random.normal(RND_MEAN,RND_STD,[10,1])
+    bias = np.zeros([1])
 
 def dataset_division():
     shuffle_map = np.arange(len(df))
     np.random.shuffle(shuffle_map)
     step_count = int(len(df) * 0.8) // 10
+
     test_begin_idx = step_count * 10
-    global train_x, train_y, test_x, test_y
-
-    train_data = df.loc[shuffle_map[:test_begin_idx]]
-    train_x = train_data.drop('Rings', axis=1)
-    train_y = train_data['Rings']
-
     test_data = df.loc[shuffle_map[test_begin_idx:]]
     test_x = test_data.drop('Rings', axis=1)
     test_y = test_data['Rings']
 
+    train_data = df.loc[shuffle_map[10*0:10*1]]
+    train_x = train_data.drop('Rings', axis=1)
+    train_x = train_x.to_numpy()
+    train_y = train_data['Rings']
+    train_y = train_y.to_numpy()
 
-def model_init():
-    weight = np.random.normal(RND_MEAN,RND_STD,[10,1])
-    bias = np.zeros([10])
+#    global weight, bias
+    output = np.matmul(train_x, weight) + bias
+    diff = output - train_y
+    square = np.square(diff)
+    loss = np.mean(square)
+    print(output, diff, loss)
 
 
 if __name__ == "__main__":
     load_dataset()
-    dataset_division()
     model_init()
+    dataset_division()
