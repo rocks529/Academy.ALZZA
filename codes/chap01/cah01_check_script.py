ac97ee8e21bc211fdc1659619587cac840629fce
BLOh <rocks529@gmail.com>
2020-03-12 01:28:58 +0900

    first output

--- codes/chap01/cah01_check_script.py
+++ codes/chap01/cah01_check_script.py
@@ -37,34 +37,66 @@ def dataset_division():
     test_data = df.loc[shuffle_map[test_begin_idx:]]
     test_x = test_data.drop('Rings', axis=1)
     test_y = test_data['Rings']
+    test_x = test_x.to_numpy()
+    test_y = test_y.to_numpy()
 
-    train_data = df.loc[shuffle_map[10*0:10*1]]
-    train_x = train_data.drop('Rings', axis=1)
-    train_x = train_x.to_numpy()
-    train_y = train_data['Rings']
-    train_y = train_y.to_numpy()
-    y = np.array([train_y]).T
+    for n in range(step_count):
+        train_data = df.loc[shuffle_map[10*0:10*1]]
+        train_x = train_data.drop('Rings', axis=1)
+        train_x = train_x.to_numpy()
+        train_y = train_data['Rings']
+        train_y = train_y.to_numpy()
+        y = np.array([train_y]).T
+        loss, acc = training(train_x, y)
+        if n % 100 == 0:
+            print("Trainingset ~ Loss: {:5.3f}, Accuracy: {:5.7f}".format(loss, acc))
 
-    output = np.matmul(train_x, weight) + bias
+    loss, acc = testing(test_x, test_y)
+    print("Testset ~ Loss: {:5.3f}, Accuracy: {:5.7f}".format(loss, acc))
 
+def testing(test_x, test_y):
+    global weight, bias
+
+    y = np.array([test_y]).T
+
+    # Y = X x W + B
+    output = np.matmul(test_x, weight) + bias
+
+    # Loss = MSE (Y-Y')
     diff = output - y
-
     square = np.square(diff)
     loss = np.mean(square)
 
+    # Accuracy
+    mdiff = np.mean(np.abs((output - y) / y))
+
+    return (loss, 1 - mdiff)
+
+def training(train_x, y):
+    global weight, bias
+    #Forward
+    # Y = X x W + B
+    output = np.matmul(train_x, weight) + bias
+
+    # Loss = MSE (Y-Y')
+    diff = output - y
+    square = np.square(diff)
+    loss = np.mean(square)
+
+    # Accuracy
     mdiff = np.mean(np.abs((output- y)/y))
 
-    print("Loss: {}, Eval: {}".format(loss, 1-mdiff))
+    #Backward
+    # dL/dw = transposeX x G
+    # dL/db = np.sum(G, axis = 0)
 
-    G_loss = 1.0
+    # G_output = dL/dY = d/dY ( MSE(Y-Y') )
+
     shape = diff.shape
     g_loss_square = np.ones(shape) / np.prod(shape)
     g_square_diff = 2 * diff
-    g_diff_output = 1
 
-    G_square = g_loss_square * G_loss
-    G_diff = g_square_diff * G_square
-    G_output = g_diff_output * G_diff
+    G_output = g_square_diff * g_loss_square
 
     g_output_w = train_x.T
 
@@ -73,9 +105,12 @@ def dataset_division():
 
     weight -= LEARNING_RATE * G_w
     bias -= LEARNING_RATE * G_b
-    print("Weight: {}, Bias: {}".format(weight, bias))
+
+    return(loss, 1-mdiff)
+
 
 if __name__ == "__main__":
     load_dataset()
     model_init()
-    dataset_division()
+    for epoch_n in range(20):
+        dataset_division()
